8738e2236bc6d5be37f1922bca7aebb0769230bb
mugagambi@gmail.com <mugagambi@gmail.com>
2019-12-12 14:20:24 +0300

    re organize files

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from core import views
+from core.views import root
 
 # API endpoints
 urlpatterns = format_suffix_patterns([
-    path('', views.api_root),
+    path('', root.api_root),
 ])
